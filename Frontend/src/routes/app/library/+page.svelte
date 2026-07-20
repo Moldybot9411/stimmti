eea837f8d2d42b6b690875674facc29d16f6fa99
Moldybot9411 <joewhite8844@gmail.com>
2026-07-20 11:08:36 +0200

    chore: take out useless library tabs

--- Frontend/src/routes/app/library/+page.svelte
+++ Frontend/src/routes/app/library/+page.svelte
@@ -24,8 +24,6 @@
 	let menuTabs = [
 		{ label: 'Surveys', icon: Form },
 		{ label: 'Sessions', icon: ChartNoAxesCombined },
-		{ label: 'Templates', icon: Plus },
-		{ label: 'Archive', icon: Archive },
 	];
 
 	let activeViewId = $state<View>((page.url.searchParams.get('view') ?? 'surveys') as View);
@@ -90,10 +88,6 @@
 				{:then sessionData}
 					<SessionList {sessionData} />
 				{/await}
-			{:else if activeViewId === 'templates'}
-				<p>Here will be the Templates</p>
-			{:else if activeViewId === 'archive'}
-				<p>Here will be the Archive</p>
 			{/if}
 		</div>
 	</div>
